531715f3db1b73344885d48ab356e0f153b3208b
InsanitySynth <72907470+InsanitySynth@users.noreply.github.com>
2020-10-20 21:41:02 +0500

    Add files via upload

---
+++ Programing/Task 1/Week1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Week1
+{
+    partial class Week1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.btnClickMe = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 9);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(39, 15);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            this.lblName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(57, 6);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(108, 23);
+            this.tbxName.TabIndex = 1;
+            this.tbxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnClickMe
+            // 
+            this.btnClickMe.Location = new System.Drawing.Point(12, 35);
+            this.btnClickMe.Name = "btnClickMe";
+            this.btnClickMe.Size = new System.Drawing.Size(153, 23);
+            this.btnClickMe.TabIndex = 2;
+            this.btnClickMe.Text = "Click Me!";
+            this.btnClickMe.UseVisualStyleBackColor = true;
+            this.btnClickMe.Click += new System.EventHandler(this.btnClickMe_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 64);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(153, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Week1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(177, 94);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClickMe);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Week1";
+            this.Text = "My First Application";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Week1_FormClosing);
+            this.Load += new System.EventHandler(this.Week1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.Button btnClickMe;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Programing/Task 1/Week1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week1
+{
+    public partial class Week1 : Form
+    {
+        public Week1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClickMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, " + tbxName.Text + ". How are you?");
+
+        }
+
+        private void Week1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Welcome to My First Application");
+        }
+
+        private void Week1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Goodbye, Cruel World");
+            e.Cancel = true;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Close();
+            Application.Exit();
+        }
+    }
+}
